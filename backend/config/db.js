7a0a7fcd5929604f5cd556eaedf77c57f9bf24c1
fari8157 <salmanul806@gamil.comqq>
2024-01-19 16:30:17 +0530

    mongo updation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,31 @@
-const mongoose =require('mongoose')
+// const mongoose =require('mongoose')
 
-const connectDB= async ()=>{
-    try {
-        const conn =await mongoose.connect(process.env.MONGO_URL,{
-            // useUnifiedTopology: true,
-            // useNewUrlParser: true,
+// const connectDB= async ()=>{
+//     try {
+//         const conn =await mongoose.connect(process.env.MONGO_URL,{
+//             // useUnifiedTopology: true,
+//             // useNewUrlParser: true,
            
-        });
-        console.log(`mongoDb connected ${conn.connection.host}`)
+//         });
+//         console.log(`mongoDb connected ${conn.connection.host}`)
+//     } catch (error) {
+//         console.error(error);
+//         process.exit(1)
+//     }
+// }
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error);
-        process.exit(1)
+        process.exit(1); // Exit with a non-zero status code to indicate an error
     }
-}
+};
+
+module.exports = connectDB;
+
 
 module.exports =connectDB
